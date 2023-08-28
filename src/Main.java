70b3c0dd566c02663505287d3b948a3c73b725b7
Solpostgit <begemot_off@mail.ru>
2023-08-28 22:13:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();/*
+        task1();
+        task1();
+        task1();
+        task1();*/
+    }
+
+    public static void task1() {
+        System.out.println("Task 1");
+    }
+}
