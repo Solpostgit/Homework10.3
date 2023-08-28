8e462c9d0e37b1ac054f9831341112d4671895cc
Solpostgit <begemot_off@mail.ru>
2023-08-28 22:15:08 +0300

    Added Task1

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ public class Main {
 
     public static void task1() {
         System.out.println("Task 1");
+        String phone = "79604503737";
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Номер телефона длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Номер телефона короткий");
+        }
+        System.out.println("phone = " + phone);
     }
 }
